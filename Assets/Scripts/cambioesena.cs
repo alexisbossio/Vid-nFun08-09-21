86c9e5e0b13ee2ef1781239a49072c580912c4c0
CpnBuzz <nicolas.alta.cba@gmail.com>
2021-09-20 15:43:49 -0300

    boton cambio de escena
    todos putos

---
+++ Assets/Scripts/cambioesena.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cambioesena : MonoBehaviour
+{
+    public enum Levels { Puerta = 0, Pinball = 1, Vidonjump = 2 }
+    public void CambioDeNivel(Levels level)
+    {
+       SceneManager.LoadScene((int)level); 
+
+    }
+    public void CambioDeNivel(int indice)
+    {
+        SceneManager.LoadScene(indice);
+    }
+}
